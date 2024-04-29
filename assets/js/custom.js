6074b3ba936f042dbd1847ca89e7ad43c81a7fa1
nigaraliyevas <naliyeva965@gmail.com>
2024-04-29 08:46:33 +0400

    some pages created

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,23 @@ function writeLocalTime() {
   dateTime.textContent = fullDate;
 }
 writeLocalTime();
+// const Swal = require("sweetalert2");
+const removeFaculties = document.querySelectorAll(".remove-faculty");
+removeFaculties.forEach((remover) => {
+  remover.addEventListener("click", function () {
+    Swal.fire({
+      title: "Silmək istədiyinizdən əminsiniz?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Bəli",
+      denyButtonText: `Xeyr`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Silindi!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Dəyişiklər yadda saxlanılmadı.", "", "info");
+      }
+    });
+  });
+});
